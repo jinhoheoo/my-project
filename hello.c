85b437b3b0db251bbbaac4f76e4bac18641ca23f
jinho Heo <hhh@gmail.com>
2024-03-15 11:31:42 +0900

    He

--- hello.c
+++ hello.c
@@ -6,7 +6,9 @@ int main()
 	printf("hello world1");
 	printf("hello world2");
 	printf("hello world4");
-	printf("hello world45");
-	printf("hello world665");
+	printf("hello world4522");
+
+
 	return 0;
+
 }
